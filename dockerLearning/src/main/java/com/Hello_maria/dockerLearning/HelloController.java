dc780a828ab07f22e55f13f9f9381eab128b21bd
TL\irfanmar <mariai@metalogicltd.co.uk>
2025-10-15 20:32:12 +0500

    create docker images. DOcker learning

---
+++ dockerLearning/src/main/java/com/Hello_maria/dockerLearning/HelloController.java
@@ -0,0 +1,14 @@
+package com.Hello_maria.dockerLearning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/name")
+	public String getName() {
+		return "Hello Maria. How are you ?";
+	}
+
+}
---
+++ dockerLearning/src/test/java/com/Hello_maria/dockerLearning/DockerLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hello_maria.dockerLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
